120740481a7e94d2a4d917caacfde113204fa811
Alex <commeau.aws@gmail.com>
2020-10-27 21:33:43 +0100

    test 3.1

--- gitlab-deploy/.gitlab-deploy.staging.sh
+++ gitlab-deploy/.gitlab-deploy.staging.sh
@@ -1,5 +1,4 @@
 # !/bin/bash
 # Get servers list:
 set — f
-git clone git@gitlab.com:commea_a2/timemanagerbackend.git && echo "clonage en cours..." \ 
-&& git checkout master && echo "docker-compose..." && docker-compose build && docker-compose-up
+git clone git@gitlab.com:commea_a2/timemanagerbackend.git && echo "clonage en cours..." && git checkout master && echo "docker-compose..." && docker-compose build && docker-compose-up
