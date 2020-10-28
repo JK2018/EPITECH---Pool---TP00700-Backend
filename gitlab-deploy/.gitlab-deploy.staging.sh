3570116c03182b2acc951bbd68556301385eeedf
Alex <commeau.aws@gmail.com>
2020-10-28 08:34:04 +0100

    test 7.1 ci

--- gitlab-deploy/.gitlab-deploy.staging.sh
+++ gitlab-deploy/.gitlab-deploy.staging.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
+
 #Get servers list
 set -f
-# string=$DEPLOY_SERVER
+string=$DEPLOY_SERVER
 array=(${string//,/ })
+
 #Iterate servers for deploy and pull last commit
 for i in "${!array[@]}"; do
-    echo "Deploy project on server ${array[i]}"
-    ssh ubuntu@ip-172-31-86-39 "cd /var/www && git pull origin master && docker-compose build && docker-compose up"
+  echo "Deploying information to EC2 and Gitlab"
+  echo "Deploy project on server ${array[i]}"
+  ssh ubuntu@${array[i]} "cd  /var/www/ && git pull origin master"
 done
